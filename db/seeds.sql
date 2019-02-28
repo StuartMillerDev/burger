4a4be49e8817ab1f962e96e9c8abb1761e689fe2
johndoeadams <johndoeadams@gmail.com>
2019-02-28 14:46:35 -0500

    initial push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);
+insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);
+insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);   
