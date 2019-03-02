d4026bd6c8d2f602be4d7ba21c569001a494fe67
johndoeadams <johndoeadams@gmail.com>
2019-03-02 17:29:15 -0500

    more

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-
+require('dotenv').config();
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: process.env.host,
+  port: process.env.port,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database
 });
 
 // Make connection.
