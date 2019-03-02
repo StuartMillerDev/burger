443c7c633a038b74ac7823c2d732ce0039ea3261
johndoeadams <johndoeadams@gmail.com>
2019-03-02 09:46:20 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);
-insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);
-insert into burgers (burger_name, devoured) values("Double Stack Ribeye w/ Bacon",false);   
+insert into burgers (burger_name, devoured) values("Single Stack Sirloin w/ Butter Sauce",false);
+insert into burgers (burger_name, devoured) values("Tomahawk burger",true);
