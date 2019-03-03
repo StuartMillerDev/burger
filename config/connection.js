d9049cb974f44e5ac73e306754551fbb34927ef4
johndoeadams <johndoeadams@gmail.com>
2019-03-02 20:39:25 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require('dotenv').config();
-var connection = mysql.createConnection({
-  host: process.env.host,
-  port: process.env.port,
-  user: process.env.user,
-  password: process.env.password,
-  database: process.env.database
-});
+var connection;
+
+if(process.env.JAWSDB_URL){
+connection=mysql.createConnection(process.end.JAWSDB_URL);
+}else {
+  connection = mysql.createConnection({
+    host: process.env.host,
+    port: process.env.port,
+    user: process.env.user,
+    password: process.env.password,
+    database: process.env.database
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
