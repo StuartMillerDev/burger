49e891bc56fd63ead4f53c90d5c929959f6c7cff
johndoeadams <johndoeadams@gmail.com>
2019-03-09 10:41:11 -0500

    update js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,41 +1,44 @@
-console.log("JS FILE LOADED");
-$("#burger_id").on("click",function(event){
 
-var id=$(this).attr("data-burger");
-  var newState = {
-        devoured: true
-      };
-      console.log("UPDATE BURGER: ",  id);
-      // Send the PUT request.
-      $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newState
-      }).then(
-        function() {
-          console.log("changed devoured to", newState);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-});
-
-$(".create-form").on("submit", function(event) {
-  // Make sure to preventDefault on a submit event.
+$(function(){
+  $(".burger_id").on("click",function(event){
   event.preventDefault();
+  var id=$(this).attr("data-burger");
+    var newState = {
+          devoured: true
+        };
+        console.log("UPDATE BURGER: ",  id);
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+          type: "PUT",
+          data: newState
+        }).then(
+          function() {
+            console.log("changed devoured to", newState);
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+  });
 
-  var newBurger = {
-    name: $("#ca").val().trim()
-  };
-console.log(newBurger);
-  // Send the POST request.
-  $.ajax("/api/burger", {
-    type: "POST",
-    data: newBurger
-  }).then(
-    function() {
-      console.log("created new burger");
-      // Reload the page to get the updated list
-      location.reload();
-    }
-  );
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim()
+    };
+  console.log(newBurger);
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
 });
